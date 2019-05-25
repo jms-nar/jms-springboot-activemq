e765a14065f019d10a87279ec7be39a6cf940660
Narendra Nirala <nknirala.007@gmail.com>
2019-05-25 23:52:13 +0530

    Jms publisher and subscriber

--- src/main/java/com/jms/jmsspringbootactivemq/JmsSpringbootActivemqApplication.java
+++ src/main/java/com/jms/jmsspringbootactivemq/JmsSpringbootActivemqApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JmsSpringbootActivemqApplication {
+	//taken from https://www.youtube.com/watch?v=zaCXYkzLgJc
 
 	public static void main(String[] args) {
 		SpringApplication.run(JmsSpringbootActivemqApplication.class, args);
